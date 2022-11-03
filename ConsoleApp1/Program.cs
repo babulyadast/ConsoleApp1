471b91a2b22da679906e4d3cf8c0d21fe4cd0380
Настя <@NESTEA>
2022-11-03 19:57:17 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double z = -0.5;
+            double x = 1.7;
+            double c = 0.44;
+
+            double v = Math.Pow(2.7,(-x * c)) * Math.Sin(z * c + x) - Math.Sqrt(Math.Abs(x * c + z));
+            double b = x * Math.Sin(Math.Pow((z * c), 2) * Math.Cos(z * c)) - 1;
+
+            Console.WriteLine("v = {0}",v);
+            Console.WriteLine("b = {0}",b);
+            Console.ReadKey();
+        }
+    }
+}
